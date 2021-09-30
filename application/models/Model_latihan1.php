7e8ffd55dfa30ec27863e692f8aa38b340f30555
dimsadingrh <dimasadingrh@gmail.com>
2021-09-30 15:45:10 +0700

    penjumlahan

--- application/models/Model_latihan1.php
+++ application/models/Model_latihan1.php
@@ -7,8 +7,8 @@ class Model_latihan1 extends CI_Model
     //method penjumlahan
     public function jumlah($nil1 = null, $nil2 = null)
     {
-        $this->nilai1 = $nil1;
-        $this->nilai2 = $nil2;
+        $this->nilai1 = $n1;
+        $this->nilai2 = $n2;
         $this->hasil = $this->nilai1 + $this->nilai2;
         return $this->hasil;
     }
