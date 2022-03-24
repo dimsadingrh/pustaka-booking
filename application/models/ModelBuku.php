bc4f40f76ef3d15445cc4bb954d6fd760fb08ebe
dimsadingrh <dimasadingrh@gmail.com>
2022-03-24 19:09:28 +0700

    WP3-P2

--- application/models/ModelBuku.php
+++ application/models/ModelBuku.php
@@ -63,12 +63,12 @@ class ModelBuku extends CI_Model
     public function updateKategori($where = null, $data = null) 
     { 
         $this->db->update('kategori', $data, $where); 
-    }
-     
+    } 
     //join 
     public function joinKategoriBuku($where) 
     { 
-        $this->db->select('buku.id_kategori,kategori.kategori'); 
+        //$this->db->select('buku.id_kategori,kategori.kategori');
+        $this->db->select('*');
         $this->db->from('buku'); 
         $this->db->join('kategori','kategori.id = buku.id_kategori'); 
         $this->db->where($where); 
