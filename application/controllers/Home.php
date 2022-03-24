723f4bfdade5fd078e8ff261f129344bd279ef4c
dimsadingrh <dimasadingrh@gmail.com>
2022-03-24 13:22:56 +0700

    pert 2

---
+++ application/controllers/Home.php
@@ -0,0 +1,17 @@
+?php
+
+class Home extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $data = [
+            'judul' => "Katalog Buku",
+            'buku' => $this->ModulBuku->getBuku()->result(),
+            ];
+    }
+}
